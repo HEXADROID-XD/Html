7e548c155aa1086e5616d6171afe08cb4a608a0f
HEXADROID-XD <jeetbag012@gmail.com>
2023-03-29 11:57:32 +0530

    new list item created

--- dom.js
+++ dom.js
@@ -1,17 +1,14 @@
-var title= document.getElementById('header-title')
-title.style.borderBottom='3px solid black'
-title.innerText="hello"
-title.style.fontFamily='courier'
-title.style.fontSize='60px'
-title.style.textAlign='center'
-
-var items=document.getElementsByClassName('title')
-items[0].innerHTML='<strong>Add Items</strong>'
-items[0].style.color='green'
-
+// 
+//using getElementsByClassName
 var list=document.getElementsByClassName("list-group-item");
-list[2].style.backgroundColor='green'
+
 for(let i=0;i<list.length;i++)
 {
-    list[i].style.fontWeight='bold'
+    list[i].style.backgroundColor='#ccc'
+}
+//usunig getElementByTagName
+var list1=document.getElementsByTagName('li')
+for(let i=0;i<list1.length;i++)
+{
+    list1[i].style.backgroundColor='#ccc'
 }
