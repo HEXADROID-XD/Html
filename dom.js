b966a94ea1f3ebb4942090f36f610ae8712289da
HEXADROID-XD <jeetbag012@gmail.com>
2023-03-29 18:18:00 +0530

    task 6

--- dom.js
+++ dom.js
@@ -1,15 +1,42 @@
-//making 2nd Item having green background cololor
-const items=document.querySelector('.list-group-item:nth-child(2)');
-items.style.backgroundColor='green'
+const itemList=document.querySelector('#items')
+// // //parent element
 
-//changing the font color of the second Item to green using querySellectorAll
-const list=document.querySelectorAll('.list-group-item');
-list[1].style.color='green'
+// console.log(itemList.parentNode.parentNode.parentNode.parentNode)
+// itemList.parentElement.style.backgroundColor='#f4f4f4'
 
-//choosing all the odd items and making the background green
-const odd=document.querySelectorAll('.list-group-item:nth-child(odd)')
-for(var i=0;i<odd.length;i++)
-{
-    odd[i].style.backgroundColor='green'
-}
+// //lastElementchild
+// itemList.lastElementChild.style.color='red'
+// //lastChild
+// console.log(itemList.lastChild)
+// itemList.lastChild.remove()
 
+//firstElementChild
+//console.log(itemList.firstElementChild)
+// itemList.firstElementChild.style.backgroundColor='red'
+//nextElementSibling
+// console.log(itemList.nextElementSibling)
+// //previousElementSibling
+// console.log(itemList.previousElementSibling)
+// itemList.previousElementSibling.innerText='ADD MORE'
+// itemList.previousElementSibling.style.fontFamily='courier'
+//create element
+const newDiv=document.createElement('div')
+//addingclass
+newDiv.className= 'hello'
+//adding id
+newDiv.id= 'hello1'
+//adding atttribute
+newDiv.setAttribute('title','gi')
+//creating textnode
+var text=document.createTextNode('HEllo')
+//adding text to new div
+newDiv.appendChild(text);
+console.log(newDiv)
+//adding this div before Item lister
+// var container=document.querySelector('header .container')
+// var h1=document.querySelector('header h1')
+// container.insertBefore(newDiv,h1)
+//adding before 1st item
+var container=document.querySelector('ul #items')
+var li=document.querySelector('ul li')
+container.insertBefore(newDiv,li)
