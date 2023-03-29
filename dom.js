b9d7a16ba22cfae88329c443a781ecfa9e4e87f1
HEXADROID-XD <jeetbag012@gmail.com>
2023-03-29 11:35:25 +0530

    background green, text bold

--- dom.js
+++ dom.js
@@ -8,3 +8,10 @@ title.style.textAlign='center'
 var items=document.getElementsByClassName('title')
 items[0].innerHTML='<strong>Add Items</strong>'
 items[0].style.color='green'
+
+var list=document.getElementsByClassName("list-group-item");
+list[2].style.backgroundColor='green'
+for(let i=0;i<list.length;i++)
+{
+    list[i].style.fontWeight='bold'
+}
