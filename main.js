f494f1134b1a2e79036b71321a0ab6d0f10bd91f
HEXADROID-XD <jeetbag012@gmail.com>
2023-03-30 22:01:23 +0530

    task 11

--- main.js
+++ main.js
@@ -18,8 +18,13 @@ function onSubmit(e) {
       setTimeout(() => msg.remove(), 3000);
     } else {
       console.log(`${nameInput.value} ${emailInput.value}`)
-      localStorage.setItem('user name',nameInput.value);
-      localStorage.setItem('user email',emailInput.value)
+      let user={
+        name:nameInput.value,
+        email:emailInput.value
+      }
+      var user_serialized=JSON.stringify(user)
+      localStorage.setItem('user details',user_serialized)
+      console.log(JSON.parse(localStorage.getItem('user details')))
     }
     
   }
