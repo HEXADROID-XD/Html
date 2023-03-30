235ad78b9ae765fdcfcb6023783ac2cc66754b33
HEXADROID-XD <jeetbag012@gmail.com>
2023-03-30 13:18:58 +0530

    task 8

--- dom.js
+++ dom.js
@@ -1,42 +1,47 @@
-const itemList=document.querySelector('#items')
-// // //parent element
+//adding items to the list and deleting them
 
-// console.log(itemList.parentNode.parentNode.parentNode.parentNode)
-// itemList.parentElement.style.backgroundColor='#f4f4f4'
+var itemList=document.querySelector('#items')
+var form=document.getElementById('addForm')
 
-// //lastElementchild
-// itemList.lastElementChild.style.color='red'
-// //lastChild
-// console.log(itemList.lastChild)
-// itemList.lastChild.remove()
+form.addEventListener('submit', onclick)
+itemList.addEventListener('click', deletion)
 
-//firstElementChild
-//console.log(itemList.firstElementChild)
-// itemList.firstElementChild.style.backgroundColor='red'
-//nextElementSibling
-// console.log(itemList.nextElementSibling)
-// //previousElementSibling
-// console.log(itemList.previousElementSibling)
-// itemList.previousElementSibling.innerText='ADD MORE'
-// itemList.previousElementSibling.style.fontFamily='courier'
-//create element
-const newDiv=document.createElement('div')
-//addingclass
-newDiv.className= 'hello'
-//adding id
-newDiv.id= 'hello1'
-//adding atttribute
-newDiv.setAttribute('title','gi')
-//creating textnode
-var text=document.createTextNode('HEllo')
-//adding text to new div
-newDiv.appendChild(text);
-console.log(newDiv)
-//adding this div before Item lister
-// var container=document.querySelector('header .container')
-// var h1=document.querySelector('header h1')
-// container.insertBefore(newDiv,h1)
-//adding before 1st item
-var container=document.querySelector('ul #items')
-var li=document.querySelector('ul li')
-container.insertBefore(newDiv,li)
+function onclick(e)
+{
+    e.preventDefault();
+    var text= document.getElementById('item');
+    var newList=document.createElement('li')
+    newList.className='list-group-item'
+    newList.appendChild(document.createTextNode(text.value));
+   
+   var deletebtn = document.createElement('button')
+   deletebtn.className='btn btn-danger btn-sm float-right delete'
+   deletebtn.appendChild(document.createTextNode("X"))
+
+newList.appendChild(deletebtn)
+   
+   itemList.appendChild(newList)
+   
+}
+function deletion(e)
+{
+      if(e.target.classList.contains('delete'))
+      {
+        if(confirm('are you sure to delete this item?'))
+        {
+            var li=e.target.parentElement;
+            li.remove()
+        }
+      }
+}
+
+ var listItem=document.querySelectorAll('li')
+ for(var i=0;i<listItem.length;i++)
+ {
+    var editbtn=document.createElement('button')
+editbtn.className='btn btn-warning btn-sm float-right delete'
+editbtn.appendChild(document.createTextNode('edit'))
+ var listItem=document.querySelectorAll('li')
+    listItem[i].appendChild(editbtn)
+    console.log(listItem[i])
+ }
