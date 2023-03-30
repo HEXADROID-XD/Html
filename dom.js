15f986b1bd4baf47dc137ada81341ddb7c5f382a
HEXADROID-XD <jeetbag012@gmail.com>
2023-03-30 21:14:58 +0530

    task 9

--- dom.js
+++ dom.js
@@ -2,22 +2,32 @@
 
 var itemList=document.querySelector('#items')
 var form=document.getElementById('addForm')
-
+var filter=document.getElementById('filter')
 form.addEventListener('submit', onclick)
 itemList.addEventListener('click', deletion)
-
+filter.addEventListener('keyup',filterItems)
 function onclick(e)
 {
     e.preventDefault();
-    var text= document.getElementById('item');
+    var text= document.getElementById('item').value;
     var newList=document.createElement('li')
     newList.className='list-group-item'
-    newList.appendChild(document.createTextNode(text.value));
+    var text1=document.getElementById('item1').value;
+    var space=document.createTextNode('1')
+    var t=document.createTextNode(text+' '+text1)
+    
+   
+
+   //  newList.appendChild(document.createTextNode(text.value));
+    newList.appendChild(t);
    
    var deletebtn = document.createElement('button')
    deletebtn.className='btn btn-danger btn-sm float-right delete'
    deletebtn.appendChild(document.createTextNode("X"))
-
+   var editbtn=document.createElement('button')
+   editbtn.className='btn btn-warning btn-sm float-right edit'
+   editbtn.appendChild(document.createTextNode('edit'))
+newList.appendChild(editbtn)
 newList.appendChild(deletebtn)
    
    itemList.appendChild(newList)
@@ -33,15 +43,51 @@ function deletion(e)
             li.remove()
         }
       }
+      if(e.target.classList.contains('edit'))
+{
+   if(confirm('are you sure to edit this?'))
+   {
+        var li=e.target.parentElement;
+        li.textContent='You just edited this feild'
+        var deletebtn = document.createElement('button')
+   deletebtn.className='btn btn-danger btn-sm float-right delete'
+   deletebtn.appendChild(document.createTextNode("X"))
+   var editbtn=document.createElement('button')
+   editbtn.className='btn btn-warning btn-sm float-right edit'
+   editbtn.appendChild(document.createTextNode('edit'))
+li.appendChild(editbtn)
+li.appendChild(deletebtn)
+   }
+}
 }
 
  var listItem=document.querySelectorAll('li')
  for(var i=0;i<listItem.length;i++)
  {
     var editbtn=document.createElement('button')
-editbtn.className='btn btn-warning btn-sm float-right delete'
+editbtn.className='btn btn-warning btn-sm float-right edit'
 editbtn.appendChild(document.createTextNode('edit'))
  var listItem=document.querySelectorAll('li')
-    listItem[i].appendChild(editbtn)
-    console.log(listItem[i])
+   // listItem[i].appendChild(editbtn)
+  // var list=document.querySelector()
+  listItem[i].insertBefore(editbtn,listItem[i].children[0])
+
  }
+
+ function filterItems(e){
+   var text= e.target.value.toLowerCase();
+   var items=itemList.getElementsByTagName('li');
+     Array.from(items).forEach(function(item){
+      var itemName=item.firstChild.textContent;
+      console.log(itemName);
+      if(itemName.toLowerCase().indexOf(text) != -1)
+      {
+         item.style.display='block';
+      }else{
+         item.style.display='none'
+      }
+     })   
+
+
+ }
+ 
