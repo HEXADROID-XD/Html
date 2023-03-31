fee9775168027cd1ab2049cd25cb1638b6c7649f
HEXADROID-XD <jeetbag012@gmail.com>
2023-03-31 16:04:52 +0530

    task 13

--- demo.js
+++ demo.js
@@ -3,9 +3,12 @@ var listUser=document.getElementById('userList')
 var userName=document.getElementById('name')
 var userEmail=document.getElementById('email')
 var userNumber=document.getElementById('Phone')
+listUser.addEventListener('click',deleteUser)
 
 form.addEventListener('submit', onCLick)
 var userCount=1;
+var localKey='hi';
+
 function onCLick(e){
     e.preventDefault();
     var user={
@@ -13,13 +16,42 @@ function onCLick(e){
         email:userEmail.value,
         PhoneNumber:userNumber.value
     }
+    var deletebtn=document.createElement('button')
+    deletebtn.className='delete'
+    var delText=document.createTextNode('delete')
+    deletebtn.appendChild(delText)
+    var liText=`Name: ${userName.value} Email : ${userEmail.value} Phone NUmber : ${userNumber.value}`
     var objText=JSON.stringify(user)
     var li=document.createElement('li')
     li.className='user-list'
-    li.appendChild(document.createTextNode(objText))
+    li.appendChild(document.createTextNode(liText))
+    li.appendChild(deletebtn)
+    //listUser.appendChild(li)
     listUser.appendChild(li)
-    var localKey='user'+userCount;
+    localKey='user'+userCount;
+    
     userCount++;
     localStorage.setItem(localKey,objText)
     
+    
+}
+function deleteUser(e){
+    if(e.target.classList.contains('delete'))
+    {   
+        var userIndex;
+        for(var i=0;i<Array.from(listUser.children).length;i++)
+        {
+            if(listUser.children[i]=== e.target.parentElement)
+            {
+                userIndex=i;
+                console.log(userIndex)
+            }
+        }
+        e.target.parentElement.remove();
+        var keyLocal='user'+(userIndex+1);
+        localStorage.removeItem(keyLocal)
+        console.log(keyLocal)
+        userCount--;
+       // console.log(localKey)
+    }
 }
